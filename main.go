f41ee17558511ef19317368e3a698ad0733020e7
Apinan Yogaratnam <72412733+apinanyogaratnam@users.noreply.github.com>
2022-02-21 14:01:19 -0500

    refactor: defined endpoint functions

--- main.go
+++ main.go
@@ -10,6 +10,43 @@ import (
 )
 
 // Book Struct
+type Book struct {
+	ID string `json:"id"`
+	Isbn string `json:"isbn"`
+	Title string `json:"title"`
+	Author *Author `json:"author"`
+}
+
+// Author Struct
+type Author struct {
+	Firstname string `json:"firstname"`
+	Lastname string `json:"lastname"`
+}
+
+// get all books
+func getBooks(w http.ResponseWriter, r *http.Request) {
+
+}
+
+// get single book
+func getBook(w http.ResponseWriter, r *http.Request) {
+	
+}
+
+// create a new book
+func createBook(w http.ResponseWriter, r *http.Request) {
+	
+}
+
+// update a book
+func updateBook(w http.ResponseWriter, r *http.Request) {
+	
+}
+
+// delete a book
+func deleteBook(w http.ResponseWriter, r *http.Request) {
+	
+}
 
 func main() {
 	// init router
