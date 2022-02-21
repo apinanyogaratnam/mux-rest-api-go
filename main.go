a1eb413e6674ada443f8181d465dc57961c4c04c
Apinan Yogaratnam <72412733+apinanyogaratnam@users.noreply.github.com>
2022-02-21 13:49:45 -0500

    refactor: created route endpoints

--- main.go
+++ main.go
@@ -1,7 +1,26 @@
 package main
 
-import("fmt")
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+	"math/rand"
+	"strconv"
+	"github.com/gorilla/mux"
+)
+
+// Book Struct
 
 func main() {
-	fmt.Println("Hello, World!")
+	// init router
+	r := mux.NewRouter()
+
+	// route handlers / endpoints
+	r.HandleFunc("/api/books", getBooks).Methods("GET")
+	r.HandleFunc("/api/books/{id}", getBook).Methods("GET")
+	r.HandleFunc("/api/books", createBook).Methods("POST")
+	r.HandleFunc("/api/books/{id}", updateBook).Methods("PUT")
+	r.HandleFunc("/api/books/{id}", deleteBook).Methods("DELETE")
+	
+	log.Fatal(http.ListenAndServe(":8000", r))
 }
